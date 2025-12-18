2d17f17a4d71f043ffe5b8ebf1f99d09a3a8d2c3
haodo04 <22130077@st.hcmuaf.edu.vn>
2025-12-18 17:43:58 +0700

    feat: add sticker & gif data lists

---
+++ src/data/gifList.js
@@ -0,0 +1,26 @@
+export const GIFS = [
+  { id: "gif_laugh_01", url: "https://media.giphy.com/media/5GoVLqeAOo6PK/giphy.gif" },
+  { id: "gif_lol_01", url: "https://media.giphy.com/media/l0HlG6D7rn3gWeu0g/giphy.gif" },
+  { id: "gif_rofl", url: "https://media.giphy.com/media/26ufdipQqU2lhNA4g/giphy.gif" },
+  { id: "gif_haha", url: "https://media.giphy.com/media/3o6Zt481isNVuQI1l6/giphy.gif" },
+
+  { id: "gif_wow_01", url: "https://media.giphy.com/media/l0HlNQ03J5JxX6lva/giphy.gif" },
+  { id: "gif_shocked", url: "https://media.giphy.com/media/3o7TKzYdJ3z0K7sKzC/giphy.gif" },
+  { id: "gif_omg", url: "https://media.giphy.com/media/10UeedrT5MIfPG/giphy.gif" },
+  { id: "gif_confused", url: "https://media.giphy.com/media/3o6ZsY8g3gV7t8D7z2/giphy.gif" },
+
+  { id: "gif_yes", url: "https://media.giphy.com/media/l0MYt5jPR6QX5pnqM/giphy.gif" },
+  { id: "gif_no", url: "https://media.giphy.com/media/26ufdipQqU2lhNA4g/giphy.gif" },
+  { id: "gif_ok", url: "https://media.giphy.com/media/111ebonMs90YLu/giphy.gif" },
+  { id: "gif_maybe", url: "https://media.giphy.com/media/l0MYC0LajbaPoEADu/giphy.gif" },
+
+  { id: "gif_angry", url: "https://media.giphy.com/media/11tTNkNy1SdXGg/giphy.gif" },
+  { id: "gif_sad", url: "https://media.giphy.com/media/3o6ZtpxSZbQRRnwCKQ/giphy.gif" },
+  { id: "gif_cry", url: "https://media.giphy.com/media/ROF8OQvDmxytW/giphy.gif" },
+  { id: "gif_sleepy", url: "https://media.giphy.com/media/26BRuo6sLetdllPAQ/giphy.gif" },
+
+  { id: "gif_hi", url: "https://media.giphy.com/media/xUA7bblcQ20SYHkYl6/giphy.gif" },
+  { id: "gif_bye", url: "https://media.giphy.com/media/l0MYyda8WjE9n8N1S/giphy.gif" },
+  { id: "gif_thank_you", url: "https://media.giphy.com/media/3oz8xIsloV7zOmt81G/giphy.gif" },
+  { id: "gif_applause", url: "https://media.giphy.com/media/3o6Zt6ML6BklcajjsA/giphy.gif" },
+];
